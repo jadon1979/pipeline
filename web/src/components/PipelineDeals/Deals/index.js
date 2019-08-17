253f8878108ef78b84fe8123ae9da92cd2e56d40
Chapley Watson <cwatson@Chapleys-MacBook-Pro.local>
2019-08-17 15:32:59 -0500

    Moved chart to shared components

--- web/src/components/PipelineDeals/Deals/index.js
+++ web/src/components/PipelineDeals/Deals/index.js
@@ -1,48 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import ApiResources from '../../Shared/ApiResources'
-import { Bar } from 'react-chartjs-2'
+import DealsBarChart from '../../Shared/DealsBarChart'
+
 
 class Deals extends Component {
-
-  barchartBackgroundColors() {
-    return [
-      'rgba(255,0,0,1)',
-      'rgba(153,51,0,1)',
-      'rgba(153,102,0,1)',
-      'rgba(153,153,0,1)',
-      'rgba(153,204,0,1)',
-      'rgba(153,255,0,1)'
-    ];
-  }
-
-  barchartHoverColors() {
-    return [
-      'rgba(153,0,102,1)',
-      'rgba(153,51,102,1)',
-      'rgba(153,102,102,1)',
-      'rgba(153,153,102,1)',
-      'rgba(153,204,102,1)',
-      'rgba(153,255,102,1)'
-    ];
-  }
-
-  buildChartData(response) {
-    const chart_details = this.mapDealsResponse(response);
-
-    return {
-      labels: Object.keys(chart_details),
-      datasets: [
-        {
-          backgroundColor: this.barchartBackgroundColors(),
-          borderColor: 'rgba(0,0,0,1)',
-          borderWidth: 1,
-          hoverBackgroundColor: this.barchartHoverColors(),
-          hoverBorderColor: 'rgba(0,0,0,1)',
-          data: Object.values(chart_details)
-        }
-      ]
-    }
-  }
 
   mapDealsResponse(response) {
     if (!response || response.length === 0) return false;
@@ -70,11 +31,8 @@ class Deals extends Component {
           path='/deals'
           render={data => {
             return (<div className="deals">
-              <Bar
-                data={this.buildChartData(data.collection)}
-                options={{
-                  legend: { display: false }
-                }}
+              <DealsBarChart
+                chart_data={this.mapDealsResponse(data.collection)}
               />
             </div>
             )
